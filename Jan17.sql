bd752de60d39766afc4fbc3696b5eb7b2d638c65
Namkhang Le <nnguyen344@gatech.edu>
2024-01-17 16:01:03 -0500

    Jan17.2

--- Jan17.sql
+++ Jan17.sql
@@ -1,4 +1,6 @@
 use company;
 select * from employee where lname like '___e%' or lname like '__e%' or lname like '_e%'; # 2nd, 3rd, or 4th is e
 
-select fname, lname, salary * 2 from employee; # Where salary is doubled
+# select fname, lname, salary * 2 from employee; # Where salary is doubled
+
+select fname, lname, salary * 2 from employee order by salary; # Where salary is doubled
